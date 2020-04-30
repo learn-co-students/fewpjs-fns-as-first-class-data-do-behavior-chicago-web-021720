a38c5dfb39919bc051e9e7087abd5b98b3c6a5ec
jequezinho <jroverby92@gmail.com>
2020-04-30 17:01:15 -0500

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,20 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function greet(estrim) {
+  const [hr, min] = estrim.split(':')
+  const timeInt = parseInt(hr) * 60 + parseInt(min)
+  if (timeInt < 60 * 12) {
+    return "Good Morning"
+  }
+  else if (timeInt <= 60 * 17) {
+    return "Good Afternoon"
+  }
+  else {
+    return "Good Evening"
+  }
+}
+
+function displayMessage(estrim) {
+  document.getElementById("greeting").innerText = estrim
+}
